39d681621ba756f71fe585cb6ec7abdd046889d5
Aayush jain <aayushjain1118@gmail.com>
2019-12-03 15:35:08 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+	res.render("search");
+});
+
+app.get("/results", function(req,res){
+	var query = req.query.search;
+	var url ="http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+	request(url, function(error,response,body){
+		if(!error && response.statusCode == 200){
+			var data = JSON.parse(body);
+			res.render("results",{data: data});	
+		}
+	});
+});
+
+app.post("/results/movie",function(req,res){
+	var imdbID = req.body.imdbID;
+	var url = "http://www.omdbapi.com/?i=" + imdbID + "&apikey=thewdb";
+	request(url, function(error,response,body){
+		if(!error && response.statusCode == 200){
+			var data = JSON.parse(body);
+			res.render("movie",{data: data});	
+		}
+	});
+});
+
+app.listen(3000,()=>{
+	console.log("server started")
+});
